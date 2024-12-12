6045e92f61f1f501e7eb8d4ac0bf072bab84fd23
Mario <jmariomarcelo@hotmail.com>
2024-12-11 19:37:35 -0500

    Ajuste Campos

--- intergrupo-MergeIFRS9/src/main/java/com/inter/proyecto_intergrupo/controller/parametric/AccountingLoadController.java
+++ intergrupo-MergeIFRS9/src/main/java/com/inter/proyecto_intergrupo/controller/parametric/AccountingLoadController.java
@@ -50,10 +50,10 @@ import java.util.stream.IntStream;
 public class AccountingLoadController {
     private static final int PAGINATIONCOUNT=5;
     private static final int PAGINATIONCOUNTDATA=500;
-    private static final String rutaArchivoFormato = "\\\\co.igrupobbva\\svrfilesystem\\BBVA_VIC06\\DP10\\Preciso\\archivo.fmt";
-    private static final String rutaArchivoFormato1 = "\\\\co.igrupobbva\\svrfilesystem\\BBVA_VIC06\\DP10\\Preciso\\";
-    //private static final String rutaArchivoFormato = "D:\\archivo.fmt";
-    //private static final String rutaArchivoFormato1 = "D:\\";
+    //private static final String rutaArchivoFormato = "\\\\co.igrupobbva\\svrfilesystem\\BBVA_VIC06\\DP10\\Preciso\\archivo.fmt";
+    //private static final String rutaArchivoFormato1 = "\\\\co.igrupobbva\\svrfilesystem\\BBVA_VIC06\\DP10\\Preciso\\";
+    private static final String rutaArchivoFormato = "D:\\archivo.fmt";
+    private static final String rutaArchivoFormato1 = "D:\\";
 
     Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
 
@@ -83,6 +83,10 @@ public class AccountingLoadController {
                 modelAndView.addObject("period",params.get("period").toString());
                 AccountingRoute ac = accountingRouteService.findById(Integer.parseInt(params.get("arhcont").toString()));
                 modelAndView.addObject("arhcont",ac);
+                if(ac!=null && ac.getNombre()!=null)
+                    modelAndView.addObject("nomb",ac.getNombre());
+                else
+                    modelAndView.addObject("nomb","Vacio");
                 logAroutes = accountingRouteService.findAllLog(ac,params.get("period").toString());
                 aroutes = accountingRouteService.findAllData(ac,params.get("period").toString(), null, null);
                 CampoRC crc= new CampoRC();
